4149ba24ed3a15c9fece14533f1e33a4d41b91a1
chi-hua <s108213042@mail1.ncnu.edu.tw>
2023-06-14 23:13:59 +0800

    wordpress id and titles

--- Flask/module/wp_titles.py
+++ Flask/module/wp_titles.py
@@ -5,7 +5,7 @@ def get_wp_titles(account,wp_password,wp_url):
     username = account
     password = wp_password
     # WordPress 網站的 URL 和 XML-RPC API 端點
-    url = 'http://'+wp_url+'/xmlrpc.php'
+    url = "http://"+wp_url+"/xmlrpc.php"
 
     # 連接到 WordPress API
     wp = xmlrpc.client.ServerProxy(url)
@@ -13,9 +13,10 @@ def get_wp_titles(account,wp_password,wp_url):
     # 獲取文章列表
     posts = wp.metaWeblog.getRecentPosts(10000, username, password)
 
-    wp_titles = []
+    wp_page = dict()
     # 遍歷文章列表，獲取標題
     for post in posts:
-        title = post['title']
-        wp_titles.append(title)
-    return wp_titles
+        title = post["title"]
+        id = post["postid"]
+        wp_page[title] = id
+    return wp_page
